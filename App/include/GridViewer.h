7231dcb874520efcf5b76be0391d9512a4486d94
Patrick S. Kanmeugne <patrick.simokanmeugne@outlook.com>
2020-10-03 14:55:58 +0200

    add drawer for grid lines

--- App/include/GridViewer.h
+++ App/include/GridViewer.h
@@ -2,14 +2,20 @@
 #define GRIDVIEWER_H
 
 #include "AbstractViewer.h"
+#include "geometry.h"
+#include <vector>
 
-class App;
+
 class GridViewer : public AbstractViewer
 {
 public:
 	GridViewer() = default;
+	virtual void initialize();
 	virtual ~GridViewer() = default;
 protected:
 	virtual void iDraw();
+private:
+	void drawLines(geometry::ISegmentFunctor&) const;
+	std::vector<geometry::Segment> _lines;
 };
 #endif // !GRIDVIEWER_H
--- App/src/GridViewer.cpp
+++ App/src/GridViewer.cpp
@@ -3,18 +3,22 @@
 #include "IGrid.h"
 #include <SFML/Graphics.hpp>
 
-class LineDrawer : public IGrid::ISegmentFunctor
+class LineDrawer : public geometry::ISegmentFunctor
 {
 private:
 	App* _app;
 	sf::Color _color;
 public :
-	LineDrawer(App* app) : _app(app), _color(sf::Color(212, 212, 212)){};
+	LineDrawer() : _color(sf::Color(212, 212, 212)){};
 	virtual ~LineDrawer() {};
-	virtual void operator()(const IGrid::Segment& segment)
+	void setApp(App* app)
 	{
-		const IGrid::Point& init = segment.first;
-		const IGrid::Point& end = segment.second;
+		_app = app;
+	}
+	virtual void operator()(const geometry::Segment& segment)
+	{
+		const geometry::Point& init = segment.first;
+		const geometry::Point& end = segment.second;
 		sf::Vertex line[] = {
 			sf::Vertex(sf::Vector2f(init.first, init.second), _color),
 			sf::Vertex(sf::Vector2f(end.first, end.second), _color)
@@ -23,8 +27,40 @@ public :
 	}
 };
 
+// segment functor for grid line drawing
+LineDrawer ld;
+
+void GridViewer::initialize()
+{
+	unsigned int sizex = _app->getGrid()->iGetSizeX();
+	unsigned int sizey = _app->getGrid()->iGetSizeY();
+	unsigned int resx = _app->getGrid()->iGetResolutionX();
+	unsigned int resy = _app->getGrid()->iGetResolutionY();
+
+	// build vertical lines
+	for (unsigned int i = 0; i <= sizex; i++)
+	{
+		geometry::Point init (std::make_pair(resx*i, 0));
+		geometry::Point end  (std::make_pair(resx*i, resy*sizey));
+		_lines.push_back(std::make_pair(init, end));
+	}
+
+	// build horizontal lines
+	for (unsigned int j = 0; j <= sizey; j++)
+	{
+		geometry::Point init (std::make_pair(0, resy*j));
+		geometry::Point end  (std::make_pair(resx*sizex, resy*j));
+		_lines.push_back(std::make_pair(init, end));
+	}
+}
+void GridViewer::drawLines(geometry::ISegmentFunctor& lf) const
+{
+	for (auto it = _lines.begin(); it != _lines.end(); it++)
+		lf(*it);
+}
+
 void GridViewer::iDraw()
 {
-	LineDrawer ld (_app);
-	_app->getGrid()->iApplyOnSegments(ld);
+	ld.setApp(_app);
+	drawLines(ld);
 }
